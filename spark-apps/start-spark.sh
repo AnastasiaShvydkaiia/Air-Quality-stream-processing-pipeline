ae7d4a75d8a042527ca13bc02aa0fae584fa8e57
Anastasia Shvydkaiia <166436183+AnastasiaShvydkaiia@users.noreply.github.com>
2025-11-27 00:27:54 +0300

    Add files via upload

---
+++ spark-apps/start-spark.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+echo "Starting Spark Streaming Job..."
+/opt/spark/bin/spark-submit \
+    --master local[*] \
+    --jars $(echo /opt/spark/jars/*.jar | tr ' ' ',') \
+    --conf "spark.driver.extraJavaOptions=-javaagent:${JMX_AGENT}=${JMX_PORT}:${JMX_CONFIG}" \
+    --conf "spark.executor.extraJavaOptions=-javaagent:${JMX_AGENT}=${JMX_PORT}:${JMX_CONFIG}" \
+    --conf "spark.mongodb.connection.uri=${MONGO_URI}" \
+    /opt/spark-apps/stream_processing.py
+
+
